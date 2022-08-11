0a836a271e0c29762e655ba75ac33825595b7b63
hamdinawfel <hamdii.nawfel@gmail.com>
2022-08-11 15:19:07 +0100

    start .NET application

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /Movies/Random
+
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "The                                                       " };
+            return View();
+        }
+
+        
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
